785d6cab46b3632c453d330b9670671461d8da06
Shams Keshk <102978841+skeshkLunchbox@users.noreply.github.com>
2022-12-02 16:06:17 +0200

    - Hide Api Key from Project source Code.
    - Hide Api Key from Project source Code.

--- app/src/main/java/com/example/nasaapp/framwork/remote/services/AsteroidsService.kt
+++ app/src/main/java/com/example/nasaapp/framwork/remote/services/AsteroidsService.kt
@@ -1,11 +1,12 @@
 package com.example.nasaapp.framwork.remote.services
 
 import com.example.core.data.PictureOfDay
+import com.example.nasaapp.BuildConfig
 import com.example.nasaapp.framwork.remote.data.RemoteAsteroidsData
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val API_KEY = "w1BfaLEMo3Bt4hzbasCM3kVNz1dUulHcc16Cr5pc"
+private const val API_KEY = BuildConfig.ASTEROIDS_API_KEY
 
 interface AsteroidsService {
 
